e5d0f1708fe4a86a765bfdac012fdb240205b088
amarnadh777 <amarnadh6565@gmail.com>
2024-09-03 23:03:38 +0530

    comment

--- frontend/src/Components/Comments/Comments.jsx
+++ frontend/src/Components/Comments/Comments.jsx
@@ -38,7 +38,7 @@ function Comments() {
   return (
     <div>
       <div className="comment">
-        {console.log("🤘🤘", context)}
+        {console.log("🤘🤘🎻📻🎶🎙🎧🎹🎷🎹", context)}
         {!context.userLogined && <h3>Login to write comment</h3>}
         <h4>Comments {`(${comment.length})`} </h4>
 
